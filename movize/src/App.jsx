f69e87496fea77f9870934c378f2a3da9e07bc07
skidev101 <skidev101@gmail.com>
2025-02-05 22:22:09 +0100

    added

--- movize/src/App.jsx
+++ movize/src/App.jsx
@@ -1,43 +1,13 @@
 import React from 'react'
-import './App.css'
 
-import LoginHeader from './Components/LoginHeader/LoginHeader'
-import SocialBtn from './Components/SocialBtn/SocialBtn'
-import Divider from './Components/Divider/Divider'
-import Inputfield from './Components/Inputfield/Inputfield'
-import Forgot from './Components/Forgot/Forgot'
-import LoginBtn from './Components/LoginBtn/LoginBtn'
-import NewUser from './Components/NewUser/NewUser'
-
-const App = () => {
+function App() {
   return (
-      <div className="container">
-          <LoginHeader />
-          
-          <SocialBtn />
-          <Divider />
-        
-        
-        <form action="#" className="login-form">
-          <Inputfield icon="fa fa-envelope"
-          type="email"
-          placeholder="Enter your email"/>
-          
-         <Inputfield icon="fa fa-lock" 
-          type="password"
-          placeholder="Enter your password"/>
-          
-          <Forgot />
-          
-          <LoginBtn />
-        </form>
-        
-        <NewUser />
-        
-      </div>
-    )
+    <div>
+      <h1 class="text-3xl font-bold underline">
+         Hello world!
+      </h1>
+    </div>
+  )
 }
 
-
 export default App
-
