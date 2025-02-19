995c14689e8398ee3f9c2c885d2599f8a8f1a905
skidev101 <skidev101@gmail.com>
2025-02-20 00:03:08 +0100

    added

--- Tiki/script.js
+++ Tiki/script.js
@@ -1,4 +1,4 @@
-let modeToggle = document.getElementById('modeToggle');
+const  modeToggle = document.getElementById('modeToggle');
 const taskInput = document.getElementById('taskInput');
 const createTaskBtn = document.getElementById('createTask');
 const  count = document.getElementById('taskCount');
@@ -15,6 +15,21 @@ let isMoonIcon = true;
 let tasks = [];
 
 let taskCount = tasks.length;
+
+const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+]
 
 if (localStorage.getItem('tasks') !== null) {
   tasks =   JSON.parse(localStorage.getItem('tasks'));
@@ -35,24 +50,35 @@ function addToHTML() {
      
      newElement.innerHTML = `
        <div class="ctask ${isMoonIcon ? '' : 'ctaskDarkMode'}" id="ctask">
-        <i class="fa-regular fa-check-circle" id="completeIcon" onClick = completeTask(${index})></i>
+        <div class="task-date">
+            <span id="t-date">${task.date}</span>
+        </div>
+        <div class="task-wrap">
+            <i class="fa-regular fa-check-circle" id="completeIcon" onClick = completeTask(${index})></i>
         <div class="task" id="task" contenteditable = "true" onInput = editTask(${index})>${task.content}</div>
         
         <i class="fa-regular fa-trash-can" id="deleteIcon" onClick = deleteTask(${index})></i>
+        </div>
         
       </div>`;
       taskList.appendChild(newElement);
     });
 }
-      
+  
      
 createTaskBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let content = taskInput.value;
     if (content !== '') {
+        let today = new Date();
+        let d = today.getDate();
+        let m = today.getMonth();
+        let y = today.getFullYear();
+        let taskDate = `${d} ${months[m]} ${y}`;
         tasks.unshift({
             content,
-            status: 'doing'
+            status: 'doing',
+            date: taskDate
         });
         
         taskCount++;
@@ -64,18 +90,20 @@ createTaskBtn.addEventListener('click', (e) => {
       
 addToHTML();
 
-function editTask(index) {
-    let edited = document.querySelectorAll('.task')[index].innerText;
-    tasks[index].content = edited;
-    saveToLocalStorage();
-}
-      
-      
 function completeTask(index) {
     tasks[index].status = 'complete';
     addToHTML();
     saveToLocalStorage();
 }   
+
+function editTask(index) {
+    let edited = document.querySelectorAll('.task')[index].innerText;
+    tasks[index].content = edited;
+    
+    saveToLocalStorage();
+}
+      
+      
 
 function deleteTask(index) {
     tasks = tasks.filter((task, newIndex) => newIndex !== index)
@@ -98,6 +126,7 @@ modeToggle.addEventListener('click', () => {
     body.classList.toggle('bodyDarkMode');
    header.classList.toggle('headerDarkMode');
     taskBox.classList.toggle('task-boxDarkMode');
+    taskInput.classList.toggle('textareaDarkMode');
      
 });
       
